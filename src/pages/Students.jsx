64f51613d248ac796255af72fc7fd31f26485bf1
Laxmipriya Behera <beheralaxmipriya725@gmail.com>
2026-08-09 17:57:52 +0530

    Add student search and filters

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Students({students, setStudents, setEditingStudent}) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [courseFilter, setCourseFilter] = useState("All");
+  const [statusFilter, setStatusFilter] = useState("All");
   const navigate = useNavigate();
   const handleDelete = (id) => {
   const updatedStudents = students.filter(
@@ -14,6 +18,24 @@ const handleEdit = (student) => {
   setEditingStudent(student);
   navigate("/add-student");
 };
+
+ const filteredStudents = students.filter((student) => {
+  const search = searchTerm.toLowerCase();
+
+  const matchesSearch =
+    student.name.toLowerCase().includes(search) ||
+    student.email.toLowerCase().includes(search);
+
+  const matchesCourse =
+    courseFilter === "All" ||
+    student.course === courseFilter;
+
+  const matchesStatus =
+  statusFilter === "All" ||
+  student.status === statusFilter; 
+
+  return matchesSearch && matchesCourse;
+});
   return (
     <div className="students-page">
 
@@ -27,6 +49,34 @@ const handleEdit = (student) => {
           + Add Student
         </button>
       </div>
+      <div className="student-filters">
+  <input
+    type="text"
+    placeholder="Search student by name or email..."
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)}
+  />
+
+  <select
+    value={courseFilter}
+    onChange={(e) => setCourseFilter(e.target.value)}
+  >
+    <option value="All">All Courses</option>
+    <option value="MCA">MCA</option>
+    <option value="BCA">BCA</option>
+    <option value="B.Tech">B.Tech</option>
+    <option value="M.Tech">M.Tech</option>
+  </select>
+
+  <select
+  value={statusFilter}
+  onChange={(e) => setStatusFilter(e.target.value)}
+>
+  <option value="All">All Status</option>
+  <option value="Active">Active</option>
+  <option value="Inactive">Inactive</option>
+</select>
+</div>
 
       <div className="student-table">
         <table>
@@ -42,13 +92,13 @@ const handleEdit = (student) => {
           </thead>
 
           <tbody>
-            {students.length === 0 ? (
+            {filteredStudents.length === 0 ? (
             <tr>
               <td colSpan="6" className="no-students">No students added yet.
               </td>
               </tr>
             ): (
-              students.map((student) =>(
+              filteredStudents.map((student) =>(
                 <tr key={student.id}>
                   <td>{student.id}</td>
                   <td>{student.name}</td>
